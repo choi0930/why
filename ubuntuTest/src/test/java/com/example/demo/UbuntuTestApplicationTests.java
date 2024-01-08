1005419cd0470b2ce45107b346e922a4aceafa32
choi09 <0930alan@gmail.com>
2024-01-08 21:17:12 +0900

    first commit

---
+++ ubuntuTest/src/test/java/com/example/demo/UbuntuTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbuntuTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
